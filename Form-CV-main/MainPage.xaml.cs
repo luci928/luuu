22ac05653b32a931528462a4260b7efd3063d3a3
luci928 <107441282+luci928@users.noreply.github.com>
2024-11-20 16:18:26 -0500

    Add files via upload

---
+++ Form-CV-main/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public string userNombre { get; set; }
+        public string userFecha { get; set; }
+        public string userOcupacion{ get; set; }
+        public string userTelefono { get; set; }
+        public string userEmail { get; set; }
+        public string userAptitud { get; set; }
+        public string userExperiencia1 { get; set; }
+        public string userExperiencia2 { get; set; }
+        public string userFormacion1 { get; set; }
+        public string userFormacion2 { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void InformacionAgregada(object sender, EventArgs e)
+        {
+            userNombre = entryNombre.Text;
+            userFecha = entryFecha.Text;
+            userOcupacion = entryOcupacion.Text;
+            userTelefono = entryTelefono.Text;
+            userEmail = entryEmail.Text;
+            userAptitud = entryAptitud.Text;
+            userExperiencia1 = entryExperiencia1.Text;
+            userExperiencia2 = entryExperiencia2.Text;
+            userFormacion1 = entryFormacion1.Text;
+            userFormacion2 = entryFormacion2.Text;
+
+            await Navigation.PushAsync(new Page1(userNombre, userFecha, userOcupacion, userTelefono, userEmail, userAptitud,userExperiencia1,userExperiencia2,userFormacion1, userFormacion2));
+        }
+
+    }
+
+
+}
---
+++ Form-CV-main/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(String nombre, String fecha, String ocupacion, String telefono, String email, String aptitud, String experiencia1, string experiencia2, String formacion1, string formacion2)
+        {
+            InitializeComponent();
+
+            labelNombre.Text = nombre + "\n" + ocupacion;
+            labelFecha.Text = "Fecha de nacimiento:\n" + fecha;
+            labelTelefono.Text = "Telefono:\n" + telefono;
+            labelEmail.Text = "Email:\n" + email;
+            labelAptitud.Text = "Soy un " + ocupacion + " titulado. Mi objetivo es ser util otorgando mis conocimientos en el area dada. Soy " + aptitud + ".\n";
+            labelExperiencia.Text = "Experiencia Laboral:\n" + experiencia1 + "\n" +experiencia2;
+            labelFormacion.Text = "Formacion Academica:\n" + formacion1 + "\n" + formacion2;
+
+        }
+
+        public Page1()
+        {
+            InitializeComponent();
+
+
+        }
+    }
+}
